27359892636bcfd24abc2f87c5344099bc130096
walid <walid.nsiri@esprit.tn>
2022-05-29 14:14:15 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import Model from './Walid';
+import Miss from './Miss';
+
+
+function App() {
+  return (
+    <Canvas
+    camera={{ position: [2, 0, 12.25], fov: 15 }}
+    style={{
+       backgroundColor: '#111a21',
+       width: '100vw',
+       height: '100vh',
+    }}
+ >
+    <ambientLight intensity={1.25} />
+    <ambientLight intensity={0.1} />
+    <directionalLight intensity={0.4} />
+    <Suspense fallback={null}>
+       <Model position={[0.025, -0.9, 0]} />
+       <Miss position={[2, -0.9, 0]}/>
+    </Suspense>
+    <OrbitControls />
+ </Canvas>
+  );
+}
+
+export default App;
